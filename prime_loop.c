386161c96c26b7021e85b97e45cb493ff112b586
saimpreetsingh01-netizen <saimpreetsingh01@gmail.com>
2025-09-17 21:58:01 +0530

    Create prime_loop.c

---
+++ prime_loop.c
@@ -0,0 +1,25 @@
+// for loop prime no or not
+#include <stdio.h>
+
+int main() {
+    int n,a,f=0;
+    printf( "enter the number");
+    scanf("%d",&n);
+    if (n<=1)
+    printf("invalid number");
+    else
+{
+    for (a=2;a<n;a++)
+{   
+    if(n % a == 0)
+    {
+        f=1;
+        break;
+    }
+}
+   if(f==0)
+     printf(" prime");
+     else
+     printf("Megatron");
+ 
+}
